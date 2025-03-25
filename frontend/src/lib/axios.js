d1b8ab051ac378266dd4e33e4691c87c3c1b4f1b
SohamAmare <sohamamare07@gmail.com>
2025-03-25 22:48:03 +0530

    Project Completed

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+//creating axios instance
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
+  withCredentials: true, //sending cookies with each request
+});
